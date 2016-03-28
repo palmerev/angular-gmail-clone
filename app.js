6873da4256aaded8eb711f4af3440a9ba47e911b
Evan Palmer <evjpalmer@gmail.com>
2016-03-28 15:05:07 -0700

    add $routeProvider call to app.config, change index.html content to ng-view

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 
-angular.module('app', ['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 function TestCtrl($scope) {
     $scope.title = 'Write a title here...';
 }
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/inbox', {
+            templateUrl: 'views/inbox.html',
+            controller: 'InboxCtrl',
+            controllerAs: 'inbox'
+        })
+        .when('inbox/email/:id', {
+            templateUrl: 'views/email.html',
+            controller: 'EmailCtrl',
+            controllerAs: 'email'
+        })
+        .otherwise({
+            redirectTo: '/inbox'
+        });
+});
